83936b8a8cebd7c7cd900a5c0908e61da4f307a1
mital-i <61635757+mital-i@users.noreply.github.com>
2022-01-05 17:41:29 -0800

    Update neighbourhood.py

--- neighbourhood.py
+++ neighbourhood.py
@@ -1,36 +1,39 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
 n = int(input())
 graph = []
-for i in range(10): 
-    line = []
-    for j in range(10): 
-        line.append('.')
+
+for i in range(102): 
+    '''    line = []
+    for j in range(30): 
+        line[j] = '.' '''
+    line = ['.']*102
     graph.append(line)
     
 for i in range(n): 
     x, y = [int(i) for i in input().split()]
     graph[x][y] = "#"
-    
-print(graph)
+
 
 def perimeter(graph): 
-    peri = None
-    def flood_fill(x, y, graph, stack, visited, length): 
+    peri = 0
+    def flood_fill(x, y, graph, visited, length): 
         nonlocal peri
-        visited.add((x, y))
-        graph[x][y]='O'
-        if peri: 
-            return 
-        if len(stack)==0: 
-            peri = length[0]
-            return 
-        for (xx, yy) in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]: 
-            if (xx, yy) not in visited and 0<xx<=10 and 0<yy<=10: 
-                if graph[xx][yy] == '#': 
-                    length[0]+=1
-                else: 
-                    flood_fill(xx, yy, graph, stack, visited, length)
+        #print(x, y, 'peri', peri)
+        if((x,y) in visited):
+            return
         
-    flood_fill(0, 0, graph, [], set(()), [0])
+        visited.add((x, y))
+        for (xx, yy) in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]: 
+            if (xx, yy) not in visited and 0<=xx<102 and 0<=yy<102: 
+                if graph[xx][yy] == '#': 
+                    peri+=1
+                else: 
+                    flood_fill(xx, yy, graph, visited, length)
+        
+    flood_fill(0, 0, graph, set(()), 0)
     return peri
     
 print(perimeter(graph))
